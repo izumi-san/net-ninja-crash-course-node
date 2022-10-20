c102d6204ca00d88b254d2860a567d460db5e65b
Cassio Izumi <izumicassio@gmail.com>
2022-10-20 14:36:36 -0300

    Added ejs

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const app = express();
 //listen to request in this port
 app.listen(3000);
 
+//register view engine
+app.set('view engine', 'ejs');
+
+const blogs = [
+  { title: 'lolzin', snippet: 'sahausuausauhsuuash' },
+  { title: 'Vavaz', snippet: 'piu piu piu' },
+  { title: 'The Office', snippet: 'kkkkkkkkkkkkkkkkkkk' },
+];
+
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
 app.get('/about-me', (req, res) => {
@@ -20,9 +29,13 @@ app.get('/express', (req, res) => {
   res.status(200).send('Olha śo eu sei usar o express');
 });
 
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create' });
+});
+
 //404 page
 // it will responde to all requests if the code reach out the bottom
 
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404.ejs', { title: 'Not Found' });
 });
